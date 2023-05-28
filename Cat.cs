1a3ccd904969d07a2b8029b0617a28680ef2f96f
zemerovaanna <123442002+zemerovaanna@users.noreply.github.com>
2023-05-28 18:24:40 +0500

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOneCreateCat
+{
+    class Cat
+    {
+        private string name;
+        private double weight;
+
+        public Cat(string CatName, double CatWeight)
+        {
+            Name = CatName;
+            Weight = CatWeight;
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+
+            set
+            {
+                bool OnlyDouble = true;
+                if(value == 0 || value < 0)
+                {
+                    OnlyDouble = false;
+                }
+
+                if(OnlyDouble == true)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    Console.Write($". Вес {value} - не может быть отрицательный или равен нулю!!");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                bool OnlyLetters = true;
+                foreach (var ch in value)
+                {
+                    if(!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.Write($"{value} - неправильное имя!!");
+                }
+            }
+        }
+        public void Meow()
+        {
+            Console.WriteLine( );
+            Console.Write($"{name}: МЯЯЯУ!");
+            Console.Write($"Мой вес: {weight}:");
+        }
+        /*public void SetCatName (string CatName)
+        {
+            bool OnlyLetters = true;
+
+            foreach ( var ch in CatName )
+            {
+                if ( !char.IsLetter(ch) )
+                {
+                    OnlyLetters = false;
+                }
+            }
+
+            if ( OnlyLetters ) name = CatName;
+            else Console.WriteLine($"{CatName} - неправильное имя!!");
+        }*/
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskOneCreateCat
+{
+    class Program
+    {
+        static void Main (string [ ] args)
+        {
+            Cat murzik = new Cat("123", -12.4);
+            Cat barsik = new Cat("Барсег", 8.8);
+
+            barsik.Meow( );
+
+            Console.ReadKey( );
+        }
+    }
+}
